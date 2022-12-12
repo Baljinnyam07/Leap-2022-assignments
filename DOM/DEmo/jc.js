c2e8c39d340909cffe94332e3d6ae2caa99ddc9d
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2022-12-12 15:50:00 +0800

    commit

---
+++ DOM/DEmo/jc.js
@@ -0,0 +1,8 @@
+// const dummyElments = document.getElementsByClassName('dummy');
+// document.getElementsByClassName('dummy')[0].innerHTML = 'hi';
+// for (let i = 0; i < dummyElments.length; i++){
+//     dummyElments[i].innerHTML = `Hello dummy div ${i+1}`;
+// }
+
+// jishee 2
+
