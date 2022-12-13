81adaa761d700269ccc1dab65a0aa0510363529f
Baljinnyam07 <baljinnyam.html@gmail.com>
2022-12-14 01:51:40 +0800

    commit

--- DOM/BOM/Settimeout/Stopwatch/js.js
+++ DOM/BOM/Settimeout/Stopwatch/js.js
@@ -1,39 +1,37 @@
 const TIMER = 10;
 let MILLISECONDS = 0;
-let SECONDS = minutes;
-let minutes = TIMER;
+let SECONDS = TIMER;
 
 const timerTarget = document.querySelector("h5");
 
 
 function updateTimer(){
-    if(MILLISECONDS <= 999){
-        SECONDS++ ;
+    if(MILLISECONDS >= 0){
+        SECONDS +;
         MILLISECONDS = 0;
     }
-    MILLISECONDS+= 1;
+    MILLISECONDS += 10;
 
-if(MILLISECONDS <=999 && SECONDS <= 999){
+if(MILLISECONDS <=0 && SECONDS <= 0){
     clearInterval(countDown);
+    gameOver();
 }
 
     let timerResult = '';
-    if(SECONDS < 10){
+    if(SECONDS => 60){
         timerResult+= "0";
     }
     timerResult+=SECONDS;
     timerResult+=":";
-    if(MILLISECONDS < 100){
+    if(MILLISECONDS = 100){
         timerResult + "0";
     }
-    if(MILLISECONDS < 10){
+    if(MILLISECONDS = 10){
         timerResult + "0";
     }
+
     timerResult += MILLISECONDS;
-
     timerTarget.innerHTML = timerResult;
-
-    
-
 }
-const  countDown = setInterval(updateTimer,1)
+
+const  countDown = setInterval(updateTimer,9)
