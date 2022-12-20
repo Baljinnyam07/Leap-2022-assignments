1c734410cef33692aa60d9049927fca2232d8a0c
Baljinnyam07 <baljinnyam.html@gmail.com>
2022-12-21 03:08:30 +0800

    commit

---
+++ Demo/Demo.js
@@ -0,0 +1,10 @@
+var newArray = [[1,1,1], [1,1,1], [1,1,1]];
+
+var logArrayOfArrays = function(array){
+   var length = array.length;
+   for(var i = 0; i < length ; i++){
+       console.log(array[i]);
+  }
+}
+
+logArrayOfArrays(newArray);
