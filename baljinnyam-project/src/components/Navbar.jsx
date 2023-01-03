3b489b61da55afe30caba57c171a107b589a3335
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-01-03 15:52:32 +0800

    commit

--- baljinnyam-project/src/components/Navbar.jsx
+++ baljinnyam-project/src/components/Navbar.jsx
@@ -1,3 +1,62 @@
+import { useState } from "react"; 
 export default function Navbar(){
-    return <>Navbar</>
+
+    const [showDropdown, setShowDropdown] = useState(false);
+    
+    const toggleDropdown = () =>{
+        setShowDropdown(!showDropdown)
+    };
+    return(<><nav className="navbar navbar-expand-lg bg-secondary">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a className="navbar-brand" href="#">Admin</a>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Нүүр</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"> </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Мэдээ</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+            <div className="dropdown text-end">
+              <a onClick={toggleDropdown} href="#" className="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown">
+                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
+              </a>
+              <ul className={`dropdown-menu text-small end-0 ${showDropdown && 'show'}`}>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    New project...
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Sign out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+  </div>
+</nav></>)
 }
