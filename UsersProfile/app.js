523621be3b6bd770f313e5e0a444462c0d10c0c2
Baljinnyam07 <baljinnyam.html@gmail.com>
2023-01-05 02:31:26 +0800

    commit

--- UsersProfile/app.js
+++ UsersProfile/app.js
@@ -1,7 +1,13 @@
-fetch('https://randomuser.me/api').then(result =>{
-    return result.json();
-})
-.then(r => {
-    // let json = JSON.parse(r);
-    console.log(r.results[0].name)
+fetch('https://randomuser.me/api').then((response)=>{
+    return response.json()
+}).then(data=>{
+    `
+    <div class="card">
+    <img src="${data.results[0].picture.large}" alt="Avatar" style="width:100%">
+    <div class="container">
+      <h4><b>John Doe</b></h4>
+      <p>Architect & Engineer</p>
+    </div>
+  </div>`
+    console.log(data.results[0].picture.large)
 })
